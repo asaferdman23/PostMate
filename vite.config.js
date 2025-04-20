1d5f7338cb6ee1108f8f3c8efbdcc0e2bf32da58
asaferdman23 <asaferdman23@gmail.com>
2025-04-20 11:44:40 +0300

    Setup Docker and GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://asaferdman23.github.io/gmail-ui/',
+  base: '/postmate/',
 })
